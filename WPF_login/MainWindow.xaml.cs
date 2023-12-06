0d8059edaf4228466fb480426fc6e9586f789b99
Владислав Кириллов <kirillovVU@cloud.biik.ru>
2023-12-06 12:33:47 +0800

    2-5 DEC 2DEC - Создан проект, создана разметка и первоначальный визуал. 3DEC - Изменен визуал, добавлены некоторые стили и анимации 4DEC - Добавлен конвертер цвета.  Выбран единый стиль проекта. 5DEC - Начало создание MVVM,

---
+++ WPF_login/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿//Theme Code ==================>>
+using MaterialDesignThemes.Wpf;
+using MySqlConnector;
+//=============================>>
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace WPF_login
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var imageBrush = new ImageBrush(new BitmapImage(new Uri("E:\\РПМ\\Задания Баинки\\WPF_login\\WPF_login\\Assets\\Images\\Fon.jpg", UriKind.Relative)));
+            this.Background = imageBrush;
+        }
+
+        
+        public bool IsDarkTheme { get; set; }
+        private readonly PaletteHelper paletteHelper = new PaletteHelper();
+        
+
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+            DragMove();
+        }
+
+        private void toggleTheme(object sender, RoutedEventArgs e)
+        {
+            
+            ITheme theme = paletteHelper.GetTheme();
+            if (IsDarkTheme = theme.GetBaseTheme() == BaseTheme.Dark)
+            {
+                IsDarkTheme = false;
+                theme.SetBaseTheme(Theme.Light);
+
+                var imageBrush = new ImageBrush(new BitmapImage(new Uri("E:\\РПМ\\Задания Баинки\\WPF_login\\WPF_login\\Assets\\Images\\Fon.jpg", UriKind.Relative)));
+                this.Background = imageBrush;
+
+                
+
+
+            }
+            else
+            {
+                IsDarkTheme = true;
+                theme.SetBaseTheme(Theme.Dark);
+
+                var imageBrush = new ImageBrush(new BitmapImage(new Uri("E:\\РПМ\\Задания Баинки\\WPF_login\\WPF_login\\Assets\\Images\\Fon2.jpg", UriKind.Relative)));
+                this.Background = imageBrush;
+
+                
+
+            }
+
+            paletteHelper.SetTheme(theme);
+            
+        }
+
+        private void exitApp(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+
+    }
+}
---
+++ WPF_login/PointConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WPF_login
+{
+    public class PointConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Length == 2 && values[0] is double x && values[1] is double y)
+            {
+                return new Point(x, y);
+            }
+
+            return DependencyProperty.UnsetValue;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
